000c4ea5bc2791ba0df0405baa3b03533fbe8f55
Douglas Hoffmann <douglas.hoffmann@quality24.com.br>
2022-10-01 16:31:36 -0400

    change all to the same file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
 import { trpc } from '../utils/trpc';
 import { NextPageWithLayout } from './_app';
-import GenericTable from '~/components/GenericTable/GenericTable';
+
+export interface GenericTableProps<T> {
+  items: T[];
+  header: () => React.ReactNode;
+  rows: (item: T) => React.ReactNode;
+}
+
+const GenericTable = <T,>({ items, header, rows }: GenericTableProps<T>) => {
+  return (
+    <>
+      {header}
+      {items.map(rows)}
+    </>
+  );
+};
 
 const IndexPage: NextPageWithLayout = () => {
   const { data } = trpc.post.list.useQuery({});
