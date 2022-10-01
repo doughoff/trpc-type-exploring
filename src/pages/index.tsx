9bec08d4709920ec55d3152c89c6b65ffda28e2e
Douglas Hoffmann <douglas.hoffmann@quality24.com.br>
2022-10-01 18:19:12 -0400

    new try

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,75 @@
+import {
+  AnyQueryProcedure,
+  inferProcedureOutput,
+  inferProcedureInput,
+} from '@trpc/server';
 import { trpc } from '../utils/trpc';
 import { NextPageWithLayout } from './_app';
+import type {
+  DecorateProcedure,
+  UseTRPCQueryOptions,
+} from '@trpc/react/shared';
 
-export interface GenericTableProps<T> {
-  items: T[];
+import { TRPCClientErrorLike } from '@trpc/client';
+
+interface GenericTableProps<
+  TProcedure extends AnyQueryProcedure,
+  TInput = inferProcedureInput<TProcedure>,
+  TData = inferProcedureOutput<TProcedure>,
+  TOptions = UseTRPCQueryOptions<
+    '',
+    inferProcedureInput<TProcedure>,
+    TData,
+    TData,
+    TRPCClientErrorLike<TProcedure>
+  >,
+> {
+  query: DecorateProcedure<TProcedure, ''>;
+  filter: TInput;
+  options: TOptions;
   header: () => React.ReactNode;
-  rows: (item: T) => React.ReactNode;
+  rows: (data: TData | undefined) => React.ReactNode;
 }
 
-const GenericTable = <T,>({ items, header, rows }: GenericTableProps<T>) => {
+const GenericTable = <
+  TProcedure extends AnyQueryProcedure,
+  TInput = inferProcedureInput<TProcedure>,
+  TData = inferProcedureOutput<TProcedure>,
+  TOptions = UseTRPCQueryOptions<
+    '',
+    inferProcedureInput<TProcedure>,
+    TData,
+    TData,
+    TRPCClientErrorLike<TProcedure>
+  >,
+>({
+  query,
+  header,
+  rows,
+  filter,
+  options,
+}: GenericTableProps<TProcedure, TInput, TData, TOptions>) => {
+  const { data } = query.useQuery(filter, options);
+
   return (
     <>
       {header}
-      {items.map(rows)}
+      {rows(data)}
     </>
   );
 };
 
-const IndexPage: NextPageWithLayout = () => {
+const LocalPage: NextPageWithLayout = () => {
   const { data } = trpc.post.list.useQuery({});
-
   return (
     <>
       <GenericTable
-        items={data?.items ?? []}
-        header={() => <span>Header</span>}
-        rows={(item) => <span>{item.title}</span>}
+        procedure={trpc.post.list}
+        filter={{}}
+        rows={(data) => <span>{data?.title}</span>}
       />
     </>
   );
 };
 
-export default IndexPage;
+export default LocalPage;
